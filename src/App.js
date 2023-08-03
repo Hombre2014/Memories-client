6ba58d57953e176988abdebd3ee69e813e21d4e7
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2023-08-03 21:46:53 +0200

    Implemented fetch a single post and post details.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Auth from './components/Auth/Auth';
 const App = () => {
   const user = JSON.parse(localStorage.getItem('profile'));
 
+  console.log('User: ', user);
+
   return (
     <GoogleOAuthProvider clientId="239350411522-uc35vv3goif01pc32m1i0ba6uc3qbd9h.apps.googleusercontent.com">
       <BrowserRouter>
@@ -20,7 +22,7 @@ const App = () => {
             <Route path="/posts" exact component={Home} />
             <Route path="/posts/search" exact component={Home} />
             <Route path="/posts/:id" exact component={PostDetails} />
-            <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
+            <Route path="/auth" exact component={() => (user === null ? <Auth /> : <Redirect to="/posts" />)} />
           </Switch>
         </Container>
       </BrowserRouter>
--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,5 +1,17 @@
-import { FETCH_ALL, START_LOADING, END_LOADING, LIKE, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_POST, START_LOADING, END_LOADING, LIKE, CREATE, UPDATE, DELETE, FETCH_BY_SEARCH } from '../constants/actionTypes';
 import * as api from '../api/index.js';
+
+export const getPost = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: START_LOADING });
+    const { data } = await api.fetchPost(id);
+
+    dispatch({ type: FETCH_POST, payload: data });
+    dispatch({ type: END_LOADING });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const getPosts = (page) => async (dispatch) => {
   try {
